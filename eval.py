c3d11c44b74a18fb63ced26a5ebe479cc3f3ee59
Yijun Quan <yijun.quan@cantab.net>
2025-05-23 10:10:22 +0100

    Fix eval.py for testing

--- eval.py
+++ eval.py
@@ -3,11 +3,11 @@ import argparse
 
 def performance_eval(dataset='sst5',teacher_eval=True, purge_eval=True, sisa_eval=True, nt=8 ,ns=2, num_epochs=1, num_slices=4, percentage=100):
     if teacher_eval:
-        teacher_evaluation(dataset=dataset, num_epochs=num_epochs, nt=nt, ns=ns)
+        teacher_evaluation(dataset=dataset, num_epochs=num_epochs, nt=nt)
     if purge_eval:
-        purge_evaluation(dataset=dataset, num_epochs=num_epochs, nt=nt, ns=ns, percentage=percentage, num_slices=num_slices)    
+        purge_evaluation(dataset=dataset, num_epochs=num_epochs, nt=nt, ns=ns, student_percentage=percentage, num_slices=num_slices)    
     if sisa_eval:
-        sisa_evaluation(dataset=dataset, num_epochs=num_epochs,nt=nt, ns=ns, percentage=percentage)
+        sisa_evaluation(dataset=dataset, num_epochs=num_epochs,nt=nt, ns=ns, student_percentage=percentage)
 
 
 def time_eval(purge_time=True, sisa_time=True):
@@ -26,14 +26,13 @@ if __name__ == "__main__":
     parser.add_argument('--num_slices', type=int, default=4, help='Number of slices (default: 4)')
     args = parser.parse_args()
     dataset = args.dataset
-    nt = args.nt
 
 
     if not os.path.isdir('./results/'):
         os.mkdir('./models/')
-    performance_eval(dataset='sst5', teacher_eval=True, purge_eval=True, sisa_eval=True, 
-                     sst_eval=True, num_epochs=args.num_epochs, nt=nt, ns=args.ns,
-                       percentage=args.percent, num_slices=args.num_slices) 
+    performance_eval(dataset=args.dataset, teacher_eval=True, purge_eval=True, sisa_eval=True, 
+                    num_epochs=args.num_epochs, nt=args.nt, ns=args.ns,
+                    percentage=args.percent, num_slices=args.num_slices) 
 
     if not os.path.isdir('./results_time/'):
         os.mkdir('./results_time/')
